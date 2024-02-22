362ee9c93ebce21719de3baf23b83cfc0117f68d
Jirka70 <jiritresohlavy2@gmail.com>
2024-02-22 14:49:55 +0100

    rfe

--- src/main/java/com/example/provoronoi/Launcher.java
+++ src/main/java/com/example/provoronoi/Launcher.java
@@ -1,10 +1,11 @@
 package com.example.provoronoi;
 
+
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-
+        System.out.println(System.getProperty("java.version"));
         Application.launch(VoronoiApp.class);
     }
 }
